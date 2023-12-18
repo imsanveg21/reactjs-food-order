beab11ddc2095bec645e16350b832ae3846e71e9
Sanveg Harsule <imsanveg21@gmail.com>
2023-12-18 19:20:14 +0530

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import CART from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
+      <CART />
       <Header />
       <main>
         <Meals />
